8718857191df3ecf9788fe2a8ad191bc579a9c65
RBestMSFT <rbest@microsoft.com>
2024-07-11 09:46:20 -0700

    update ade cmds

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,7 @@ echo -e "\n>>> Terraform Info...\n"
 terraform -version
 ade log --content "AAAAAAAAA"
 ade log --type error --content "BBBBBB"
-ade log --content "This is a special log." --file "specialLogFile.txt"
+# ade log --type verbose --content "This is a special log." --file "specialLogFile.txt"
 
 echo -e "\n>>> Initializing Terraform...\n"
 terraform init -no-color
